130c36a0c267e894d7ec464760489aa74d7c7d36
Vlad Krupen <air7360@gmail.com>
2024-09-18 10:12:37 +0300

    Initial commit

---
+++ FruitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruitHub
+//
+//  Created by Vlad on 18.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
